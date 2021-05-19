8034d090e12c810f293cc1b769f17b8345c2ae48
lecklis <1327970561@qq.com>
2021-05-19 16:16:03 +0800

    disnaci

--- 8.py
+++ 8.py
@@ -1,1 +1,16 @@
-ffdf
+print("fffff")
+# vscode-git中的U,M和D文件标记含义
+
+# Anish551 2019-09-02 21:27:45  11645  收藏 21
+# 分类专栏： Tools
+# 版权
+# 20190902211505.png
+
+# M modified
+# 你已经在github中添加过该文件，然后你对这个文件进行了修改，就会文件后标记M
+# U untracked
+# 你在本地新建了这个文件，还未提交到github上，就会标记U
+# D delete
+# 你删除了这个文件，vscode-git会记录下这个状态
+# 6,U
+# 表示有6个错误且untracked
